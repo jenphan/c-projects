88edf51ecc356c4f3cd413ac5e13b755e3f147b5
Jen <jdp386@drexel.edu>
2023-08-07 22:13:39 -0400

    Add actual new validation functions

--- calculator.c
+++ calculator.c
@@ -1,62 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void clear_input_buffer();
 void print_error(const char *message);
 void print_menu();
+int get_valid_operation();
+float get_valid_float(const char *prompt);
 float calculate_result(int op, float num1, float num2);
 
 int
 main()
 {
-    int op, c;
+    int op;
     float num1, num2, result;
 
     while(1) {
         system("clear");
         print_menu();
 
-        do {
-            printf("Please enter the operation option: ");
-            if (scanf("%d", &op) != 1) {
-                print_error("Invalid operation option selected");
-            } else if (op < 1 || op > 5) {
-                print_error("Invalid operation option selected");
-            } else {
-                break;
-            }
-        } while (1);
+        op = get_valid_operation();
         
         if (op == 5) {
             printf("Exiting calculator program.\n");
             break;
         }
 
-        while ((c = getchar()) != '\n' && c != EOF);
-        do {
-            printf("Please enter the first number: ");
-            if (scanf("%f", &num1) != 1) {
-                print_error("Invalid value input for first number");
-                continue;
-            } else {
-                break;
-            }
-        } while (1);
+        num1 = get_valid_float("Please enter the first number: ");
 
-        while ((c = getchar()) != '\n' && c != EOF);
-        do {
-            printf("Please enter the second number: ");
-            if (scanf("%f", &num2) != 1) {
-                print_error("Invalid value input for second number");
-            } else if (num2 == 0 && op == 4) {
-                print_error("Cannot divide by zero");
-            } else {
-                break;
-            }
-        } while (1);
+        if (op == 4) {
+            do {
+                num2 = get_valid_float("Please enter the second number: ");
+                if (num2 == 0 && op == 4) {
+                    print_error("Cannot divide by zero");
+                } else {
+                    break;
+                }
+            } while (1);
+        } else {
+            num2 = get_valid_float("Please enter the second number: ");
+        }
 
         result = calculate_result(op, num1, num2);  
         printf("Result: %.2f\n", result);
-        while ((c = getchar()) != '\n' && c != EOF);
+        clear_input_buffer();
         printf("\nPress anything to continue: ");
         getchar();
     }
@@ -64,12 +50,18 @@ main()
 }
 
 void
-print_error(const char *message)
+clear_input_buffer()
 {
     int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void
+print_error(const char *message)
+{
     fprintf(stderr, "Error: %s\n", message);
     printf("\n");
-    while ((c = getchar()) != '\n' && c != EOF);
+    clear_input_buffer();
 }
 
 void
@@ -81,6 +73,40 @@ print_menu()
             "3. Multiplication\n"
             "4. Division\n"
             "5. Exit\n");
+}
+
+int
+get_valid_operation()
+{
+    int op;
+    do {
+        printf("Please enter the operation option: ");
+        if (scanf("%d", &op) != 1) {
+            print_error("Invalid operation option selected");
+        } else if (op < 1 || op > 5) {
+            print_error("Invalid operation option selected");
+        } else {
+            break;
+        }
+    } while (1);
+    clear_input_buffer;
+    return op;
+}
+
+float
+get_valid_float(const char *prompt)
+{
+    float num;
+    do {
+        printf("%s", prompt);
+        if (scanf("%f", &num) != 1) {
+            print_error("Invalid value input for the number");
+        } else {
+            break;
+        }
+    } while (1);
+    clear_input_buffer;
+    return num;
 }
 
 float

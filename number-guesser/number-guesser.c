fba66cd2c3d71e7ee4a275e13417d3093c5c0a87
Jen <jdp386@drexel.edu>
2023-08-18 16:37:26 -0400

    Add play again feature

--- number-guesser/number-guesser.c
+++ number-guesser/number-guesser.c
@@ -5,6 +5,7 @@
 
 int generate_number();
 int player_guess();
+int play_again();
 
 int
 main()
@@ -31,19 +32,13 @@ main()
                 if (secret == guess) {
                     points += 1;
                     fprintf(stdout, "You correctly guessed the secret number %d in %d attempt(s)!\n", secret, attempts);
-                    fprintf(stdout, "DO you want to play again (yes/no): ");
-                    fgets(again, sizeof(again), stdin);
-                    again[strcspn(again, "\n")] = '\0';
-
-                    if (strcmp(again, "no") == 0) {
+                    if (!play_again()) {
                         fprintf(stdout, "Thank you for playing! Your score is %d.\n", points);
                         return 0;
-                    } else if (strcmp(again, "yes") != 0) {
-                        fprintf(stdout, "Invalid input. Playing another game.\n");
+                    } else {
+                        secret = generate_number();
+                        attempts = 0;
                     }
-
-                    secret = generate_number();
-                    attempts = 0;
                 } else if (secret > guess) {
                     fprintf(stdout, "The secret number is HIGHER than your guess %d.\n", guess);
                 } else {
@@ -83,4 +78,20 @@ player_guess()
             return guess;
         }
     }
+}
+
+int
+play_again()
+{
+    char input[10];
+    size_t len, i;
+
+    fprintf(stdout, "Do you want to play again (yes/no): ");
+    fgets(input, sizeof(input), stdin);
+    len = strlen(input);
+
+    if (len > 0 && input[len - 1] == '\n') {
+        input[len - 1] = '\0';
+    }
+    return (strcmp(input, "yes") == 0);
 }

7e9d849ed64aefaa816bc04814fd1db7d7ed772a
Jen <jdp386@drexel.edu>
2023-08-18 18:15:53 -0400

    Refactor functionality and case-insensitive input

--- palindrome-checker/palindrome-checker.c
+++ palindrome-checker/palindrome-checker.c
@@ -1,33 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+#define MAX_WORD_LENGTH 50
+
+void check_palindrome(const char* word);
 
 int
 main()
 {
-    char word[50], reverse[50], again;
-    int length, i;
+    char word[MAX_WORD_LENGTH], again;
 
     printf("(´• ω •`) ♡ PALINDROME CHECKER (´ε｀ )♡\n");
     do {
         printf("\nPlease enter a single word to check: ");
         scanf("%s", word);
-        length = strlen(word);
         while (getchar() != '\n');
 
-        for (i = 0; i < length; i++) {
-            reverse[i] = word[length - 1 - i];
-        }
-        reverse[length] = '\0';
-
-        if (strcmp(word, reverse) == 0) {
-            printf("%s IS a palindrone! :)\n", word);
-        } else {
-            printf("%s IS NOT a palindrone :(\n", word);
-        }
+        check_palindrome(word);
 
         printf("\nWould you like to check another word (y/n)? ");
         while ((again = getchar()) == '\n');
-    } while (again == 'y' || again == 'Y');
+    } while (tolower(again) == 'y');
+
     printf("\nThank you for using the palindrome checker. Goodbye!\n");
-   return 0;
+    return 0;
+}
+
+void
+check_palindrome(const char *word)
+{
+    char reversed[MAX_WORD_LENGTH];
+    int length, i;
+
+    length = strlen(word);
+
+    for (i = 0; i < length; i++) {
+        reversed[i] = word[length - 1 - i];
+    }
+
+    reversed[length] = '\0';
+
+    if (strcasecmp(word, reversed) == 0) {
+        printf("%s IS a palindrome! :)\n", word);
+    } else {
+        printf("%s IS NOT a palindrome :(\n", word);
+    }
 }

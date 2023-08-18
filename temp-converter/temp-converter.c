336da72c67d44910c9a1252726bedb3b4bfcaf7b
Jen <jdp386@drexel.edu>
2023-08-18 17:34:05 -0400

    Add proper error handling and program loop

--- temp-converter/temp-converter.c
+++ temp-converter/temp-converter.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 void convert_f_to_c(double f);
 void convert_c_to_f(double c);
@@ -8,50 +9,47 @@ main()
 {
     double original;
     char unit, again;
-    int valid;
 
     printf("(´• ω •`) ♡ TEMPERATURE CONVERTER (´ε｀ )♡\n");
 
     do {
         printf("Please enter the temperature you would like converted: ");
-        scanf("%lf", &original);
-        while ((getchar()) != '\n');
+        while (scanf("%lf", &original) != 1 || getchar() != '\n') {
+            printf("Invalid input. Please enter a valid temperature: ");
+            while ((getchar()) != '\n');
+        }
 
-        valid = 0;
         do {
             printf("Please enter what unit to convert to (f/c): ");
             unit = getchar();
+            unit = tolower(unit);
 
             if (unit == 'f') {
                 convert_c_to_f(original);
-                valid = 1;
             } else if (unit == 'c') {
                 convert_f_to_c(original);
-                valid = 1;
-            } else {
-                fprintf(stderr, "Please enter what unit to convert to (f/c): ");
             }
             while ((getchar()) != '\n');
-        } while (!valid);
+        } while (unit != 'f' && unit != 'c');
 
-        printf("Convert another temperature (y/n)? ");
-        again = getchar();
-        while ((getchar() != '\n'));
+        printf("\nConvert another temperature (y/n)? ");
+        while ((again = getchar()) == '\n');
     } while (again == 'y');
+
+    printf("\nThank you for using the temperature converter! Goodbye!\n");
+    return 0;
 }
 
 void
 convert_f_to_c(double f)
 {
-    double c;
-    c = ((f - 32) * 5) / 9;
+    double c = ((f - 32) * 5) / 9;
     printf("%.2f°F is %.2f°C\n", f, c);
 }
 
 void
 convert_c_to_f(double c)
 {
-    double f;
-    f = ((c * 9) / 5) + 32;
+    double f = ((c * 9) / 5) + 32;
     printf("%.2f°C is %.2f°F\n", c, f);
 }

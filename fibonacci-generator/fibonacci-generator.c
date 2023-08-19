f5fef45bcbb931a4a0b6991e3b2e642059f24c25
Jen <jdp386@drexel.edu>
2023-08-19 12:04:40 -0400

    Refactor for efficiency

--- fibonacci-generator/fibonacci-generator.c
+++ fibonacci-generator/fibonacci-generator.c
@@ -10,25 +10,21 @@ main()
     char repeat;
 
     printf("(´• ω •`) ♡ FIBONACCI GENERATOR (´ε｀ )♡\n");
+
     do {
         printf("\nPlease enter number of terms to generate: ");
         while (scanf("%d", &number) != 1) {
-            printf("Invalid input. Please enter a valid integer. ");
+            printf("Invalid input. Please enter a valid integer: ");
             while (getchar() != '\n');
         }
 
-        int sequence[number];
         for (int i = 0; i < number; i++) {
-            sequence[i] = fibonacci(i);
-        }
-
-        for (int i = 0; i < number; i++) {
-            printf("%d ", sequence[i]);
+            printf("%d ", fibonacci(i));
         }
 
         printf("\n\nWould you like to generate another sequence (y/n)? ");
         while ((repeat = tolower(getchar())) == '\n');
-    } while (repeat == 'y' || repeat == 'Y');
+    } while (repeat == 'y');
 
     printf("\nThank you for using Fibonacci Generator. Goodbye!\n");
     return 0;
@@ -37,21 +33,21 @@ main()
 int
 fibonacci(int number)
 {
-    int sequence[number];
-
     if (number <= 0) {
         return 0;
-    }
-
-    if (number == 1) {
+    } else if (number == 1) {
         return 1;
     }
 
-    sequence[0] = 0;
-    sequence[1] = 1;
+    int prev = 0;
+    int current = 1;
+    int result = 0;
 
     for (int i = 2; i <= number; i++) {
-        sequence[i] = sequence[i - 1] + sequence[i - 2];
+        result = prev + current;
+        prev = current;
+        current = result;
     }
-    return sequence[number];
+
+    return result;
 }

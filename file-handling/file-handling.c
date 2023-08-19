461e7237e397c9f3ec00cea7c1be5ccd398f24e1
Jen <jdp386@drexel.edu>
2023-08-19 17:37:34 -0400

    Refactor for readability and efficiency

--- file-handling/file-handling.c
+++ file-handling/file-handling.c
@@ -1,41 +1,65 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_NUMBERS 100
+
 void print_menu();
-double perform_operation(int op, int *numbers, int count);
+double calculate_sum(int *numbers, int count);
+double find_maximum(int *numbers, int count);
+double find_minimum(int *numbers, int count);
 
 int
 main()
 {
     FILE *file;
-    char fileName[100];
-    double result;
-    int numbers[100], operation, c;
+    char fileName[256];
+    int numbers[MAX_NUMBERS], operation, count = 0;
+    double result = 0;
 
     printf("(´• ω •`) ♡ FILE HANDLING PROGRAM (´ε｀ )♡\n");
     printf("Please enter the name of the file: ");
     fgets(fileName, sizeof(fileName), stdin);
-    fileName[strcspn(fileName, "\n")] = '\0';
+    sscanf(fileName, "%255[^\n]", fileName);
 
     file = fopen(fileName, "r");
     if (file == NULL) {
-        printf("File %s not found. Please enter a valid file name.", fileName);
+        printf("File %s not found. Please enter a valid file name. ", fileName);
         return 1;
     }
 
-    while (c < 100 && fscanf(file, "%d", &numbers[c]) == 1) {
-        c++;
+    while (count < MAX_NUMBERS && fscanf(file, "%d", &numbers[count]) == 1) {
+        count++;
     }
     fclose(file);
 
     print_menu();
     printf("Please enter a valid operation (1 - 5): ");
-    if (scanf("%d", &operation) != 1) {
-        printf("Invalid input. Please enter a valid operation: ");
+    if (scanf("%d", &operation) != 1 || operation < 1 || operation > 5) {
+        printf("Invalid input. Please enter a valid operation.");
         return 1;
     }
     
-    result = perform_operation(operation, numbers, c);
+    switch(operation)
+    {
+        case 1:
+            result = calculate_sum(numbers, count);
+            break;
+        case 2:
+            result = calculate_sum(numbers, count) / count;
+            break;
+        case 3:
+            result = find_maximum(numbers, count);
+            break;
+        case 4:
+            result = find_minimum(numbers, count);
+        case 5:
+            printf("\nThank you for using the File Handling Program. Goodbye!\n");
+            return 0;
+        default:
+            printf("Invalid operation selected.\n");
+            return 1;
+    }
+
     printf("\nResult: %.2f\n", result);
 
     return 0;
@@ -53,39 +77,35 @@ print_menu()
 }
 
 double
-perform_operation(int op, int *num, int count)
+calculate_sum(int *numbers, int count)
 {
-    double result = 0;
-    int i;
-    switch (op)
-    {
-        case 1:
-            for (i = 0; i < count; i++) {
-                result += num[i];
-            }
-            break;
-        case 2:
-            for (i = 0; i < count; i++) {
-                result += num[i];
-            }
-            result = result / count;
-            break;
-        case 3:
-            result = num[0];
-            for (i = 1; i < count; i++) {
-                if (num[i] > result) {
-                    result = num[i];
-                }
-            }
-            break;
-        case 4:
-            result = num[0];
-            for (i = 1; i < count; i++) {
-                if (num[i] < result) {
-                    result = num[i];
-                }
-            }
-            break;
+    double sum;
+    for (int i = 0; i < count; i++) {
+        sum += numbers[i];
     }
-    return result;
+    return sum;
+}
+
+double
+find_maximum(int *numbers, int count)
+{
+    double maximum = numbers[0];
+    for (int i = 1; i < count; i++) {
+        if (numbers[i] > maximum) {
+            maximum = numbers[i];
+        }
+    }
+    return maximum;
+}
+
+double
+find_minimum(int *numbers, int count)
+{
+    double minimum = numbers[0];
+    for (int i = 1; i < count; i++) {
+        if (numbers[i] < minimum) {
+            minimum = numbers[i];
+        }
+    }
+    return minimum;
 }
